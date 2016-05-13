e2231a2003d3b23217ad44cf5fda3906d1badf54
NemusLoren <nemusloren@gmail.com>
2016-05-13 14:40:01 +0200

    v0.3
    Add loop(conte);

---
+++ testScript.js
@@ -0,0 +1,48 @@
+$( "body" ).append( "<p style='width: 100px;he;height: 30px;position: fixed;right: 0;top: 0;background-color: white;z-index: 999;color: black;text-align: center;'>Likes: <span id='lk00'>0</span> </p>" );
+var conte = 0;
+(function loop(conte) {
+	if (typeof conte == 'undefined'){
+		var conte = 1;
+	}else{
+		conte++;
+	}
+
+	
+	$( "#lk00" ).empty();	
+	$( "#lk00" ).html(conte);		
+
+    var rand = Math.round(Math.random() * (5000 - 500)) + 10000;
+    setTimeout(function() {
+		
+ $('._jveic._rhgel > a')[0].click();
+		setTimeout(function(){
+			$('.coreSpriteRightPaginationArrow')[0].click();
+}, 2000);
+
+	/*if(conte == 100){
+		setpausa(function(){}, 360000);
+	}	
+	*/
+		
+		loop(conte);  
+		
+    }, rand);
+}());
+
+
+
+/*var e = jQuery.Event("keydown");
+e.which = 39; // # Some key code value
+$("body").trigger(e);
+
+function simulateKeyPress(character) {
+  jQuery.event.trigger({ type : 'keypress', which : character.charCodeAt(0) });
+}
+
+$(function() {
+  $('body').keypress(function(e) {
+    alert(e.which);
+  });
+
+  simulateKeyPress("e");
+});*/
